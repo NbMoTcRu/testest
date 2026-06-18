53addc21cb273bd798d67c3c24a147aa094c6970
NbMoTcRu <maeda.study489@gmail.com>
2026-06-18 22:51:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.set_page_config(page_title="ログイン",page_icon="👀")
 st.header("ログイン")
 email = st.text_input(label="メールアドレス",placeholder="Hello123@example.com")
 if not email:
