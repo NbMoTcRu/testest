076951d46d6f6a9ae69cb2ce460802d3e2dcb752
NbMoTcRu <maeda.study489@gmail.com>
2026-06-18 20:57:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
 import streamlit as st
 
 st.title("Create Your Account")
-
-
-"""
-st.text_input("yaa",4,4)
-st.checkbox("aaa")
-st.selectbox("hey",[1,2,3,4,5])
-if st.button("iii"):
-    st.write("yayaya")
-"""
